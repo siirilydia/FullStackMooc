946791032cfa3aa9d6fc0f53ee6408fe09d6afe4
Siiri Helin <siiri.lydia@hotmail.com>
2020-06-17 14:50:16 +0300

    tehtävä 1.1 tehty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (props) => {
+  return (
+    <h1>
+    {props.headertext}
+    </h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+// Ajattelin välittää Totaliin vain yhtenä propsina arrayn, joka sisältää
+// kaikki osioiden harjoitusten määrät, mutta en lähde nyt keulimaan
+// jos koodia ollaan kuitenkin refaktoroimassa myöhemmissä harjoituksissa.
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.exc1+props.exc2+props.exc3}
+    </p>
+  )
+}
+
 const App = () => {
   const course = 'Half stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +40,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header headertext={course}/>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exc1={exercises1} exc2={exercises2} exc3={exercises3}/>
     </div>
   )
 }
