c0bc31365de75fab13eb976c37d4d5ecd833ed40
Siiri Helin <siiri.lydia@hotmail.com>
2020-06-29 16:42:02 +0300

    tehtävät 1.8 ja 1.9 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -4,34 +4,61 @@ import ReactDOM from 'react-dom'
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
+  
+const Statistics = (props) => {
+    
+  const average = () => {
+    let total = 0
+
+    props.allRatings.forEach(rating => {
+      total += rating
+    })
+
+    return (
+      total/props.allRatings.length
+    )
+  }
+      
+  const goodReviews = () => {
+    let goodAmount = 0
+
+    props.allRatings.forEach(rating => {
+      if (rating === 1){
+        goodAmount++
+      }
+    })
+
+    return goodAmount/props.allRatings.length*100
+  }
+      
+  if (props.allRatings.length === 0) {
+    return (
+      <div>
+        <br/>No feedback given
+      </div>
+    )
+  }
+
+  return(
+    <div>
+      <h1>statistics</h1>
+      good {props.good}
+      <br/>neutral {props.neutral}
+      <br/>bad {props.bad}
+      <br/>all {props.allRatings.length}
+      <br/>average {average()}
+      <br/>positive {goodReviews()}%
+    </div>
+  )
+}
 
 const App = () => {
-  //tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [allRatings, setRating] = useState([])
   
-  const average = () => {
-    let total = 0
-    allRatings.forEach(rating => {
-      total += rating
-    })
-    return (
-       total/allRatings.length
-      )
-  }
-
-  const goodReviews = () => {
-    let goodAmount = 0
-    allRatings.forEach(rating => {
-      if (rating === 1){
-        goodAmount++
-      }
-    })
-    return goodAmount/allRatings.length*100
-  }
-
+  
   const handleGoodClick = () => {
     setGood(good +1 )
     setRating(allRatings.concat(1))
@@ -53,13 +80,7 @@ const App = () => {
       <Button handleClick={() => handleGoodClick()} text="good" />
       <Button handleClick={() => handleNeutralClick()} text="neutral" />
       <Button handleClick={() => handleBadClick()} text="bad" />
-      <h1>statistics</h1>
-      good {good}
-      <br/> neutral {neutral}
-      <br/> bad {bad}
-      <br/>all {allRatings.length}
-      <br/>average {allRatings.length !== 0 ? average() : null}
-      <br/>positive {allRatings.length !== 0 ? goodReviews() : null}%
+      <Statistics good={good} neutral={neutral} bad={bad} allRatings={allRatings}/>
     </div>
   )
 }
